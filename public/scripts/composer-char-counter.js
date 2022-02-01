d2ca65faf8c301307a1e5be50e0fc1448099e0e0
Manseerat Kaur <seeratsidhu01@gmail.com>
2022-02-01 16:56:46 -0500

    update the counter on page as user types tweet

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-  
+  $("#tweet-text").on('keyup', function() {
+    const text = $(this).val();
+    const form = $(this).parents().eq(1);
+    const counter = $(form).children('.form-end').children('.counter');
+    const charsLeft = (140 - text.length).toString();
+    $(counter).html(charsLeft);
+  });
 });
+
