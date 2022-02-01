3518d08ac4cb211080f51a6e3b7b89f210cb21a2
Manseerat Kaur <seeratsidhu01@gmail.com>
2022-02-01 17:11:22 -0500

    counter turns red on exceeding word limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,6 +5,12 @@ $(document).ready(function() {
     const counter = $(form).children('.form-end').children('.counter');
     const charsLeft = (140 - text.length).toString();
     $(counter).html(charsLeft);
+
+    if (charsLeft < 0) {
+      $(counter).addClass('invalid');
+    } else {
+      $(counter).removeClass('invalid');
+    }
   });
 });
 
